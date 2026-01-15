653792fa02d2a44fd9fda67f6a5186bfb0fd4b71
kavancamp <keenahvancampenhout@gmail.com>
2026-01-15 13:32:28 -0600

    added script to get houdini path

---
+++ getHoudiniPath.py
@@ -0,0 +1,16 @@
+import sys
+import pathlib
+import json
+
+vscode_settings = {}
+
+pythonexe_path = pathlib.Path(sys.prefix).resolve().joinpath('python.exe')
+vscode_settings['python.defaultInterpreterPath'] = pythonexe_path.as_posix()
+
+sys_paths = [pathlib.Path(p).resolve() for p in sys.path]
+vscode_settings['python.analysis.extraPaths'] = [p.as_posix() for p in sys_paths]
+
+print(json.dumps(vscode_settings, indent=4))
+
+
+# copy and paste into houdini python node, copy the output from the console and paste to vscode settings>Python>autocomplete.extraPaths (edit in json )
